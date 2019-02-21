584eacc1a01998b584a190dadb6b408d8a1519c3
verrixkio <verrixkio@gmail.com>
2019-02-21 23:17:26 +0000

    Added AJAX functionality into the submit button to begin DOM manipulation with tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,45 +6,112 @@ $(document).ready(function() {/*
  console.log('ready for work')
 
   // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": {
+          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+        },
+        "handle": "@SirIsaac"
       },
-      "handle": "@SirIsaac"
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": {
+          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        },
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
     },
-    "created_at": 1461116232227
+    {
+      "user": {
+        "name": "Johann von Goethe",
+        "avatars": {
+          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+  ];
+
+  function createTweetElement(tweetInfo) {
+ 
+      //Okay we need small profile picture
+    const profilePic = tweetInfo.user.avatars.small
+    //Username
+    const tweetUserName = tweetInfo.user.name
+    console.log(tweetUserName)
+    //Handle
+    const handle = tweetInfo.user.handle
+    //date
+    const tweetDate = tweetInfo.created_at
+    //tweet
+    const tweetContent = tweetInfo.content.text
+    return (`
+      <article class="tweet">
+              <header>
+                <img class="profile-pic" src="${profilePic}"</img>
+                <span class="profile-name">${tweetUserName}</span>
+                <span class="user-handle">${handle}</span>
+              </header>
+              <div>
+                <span class="tweet">${tweetContent}</span>
+              </div>
+              <footer>
+                <span class="date">${tweetDate}</span>
+              </footer>
+      </article>
+    
+    `)
+
+    
   }
+  function renderTweets(tweets) {
+    // loops through tweets
+    for (tweet in tweets) {
+      
+      // calls createTweetElement for each tweet
+      const $tweet = createTweetElement(tweets[tweet])
+      // takes return value and appends it to the tweets container
+      $('.container').append($tweet);
+    }
+  }
+  renderTweets(data)
 
-  //First thing i need to do is learn how to post something to the dom.
-  //Lets start with building a basic container on the dom
-    //Wrong dom edits and appends
+  var $form = $('#form-tweet');
+  $form.on('submit', function (event) {
+    console.log('Button clicked, performing ajax call...');
+    event.preventDefault()
+    const serialtweet = $(this).serialize()
+    $.ajax('/tweets', { method: 'POST', data: serialtweet })
+      console.log(serialtweet)
+    //.then(function (tweetAsync) {
+      //console.log(serialtweet)
+      //$button.replaceWith(morePostsHtml);
+    });
+  });
 
-  //Lets get the info from the tweetData
-  //Okay we need small profile picture
-  const profilePic = tweetData.user.avatars.small
-  //Username
-  const tweetUserName = tweetData.user.name
-  //Handle
-  const handle = tweetData.user.handle
-  //date
-  const tweetDate = tweetData.created_at
-  //tweet
-  const tweetContent = tweetData.content
 
-  // start by adding the username too...
-    //path = 
-  $('.container article header .profile-name').text(tweetUserName)
-  // I need to create this element via a path structure.
-  
-
-});
+// });
 
 
 
