f7116423f116fdd0b648642a01833df4f19fc876
verrixkio <verrixkio@gmail.com>
2019-02-22 22:19:09 +0000

    committing small change to begin altering the database structure.

--- server/lib/in-memory-db.js
+++ server/lib/in-memory-db.js
@@ -2,6 +2,9 @@
 
 // Requiring a JSON file automatically parses it and returns the data. These
 // are just example tweets to make it less tedious to style the app initially.
+
+
+//This may be where I need to require my database from!
 const db = {
   tweets: require("../data-files/initial-tweets")
 }
--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -7,23 +7,29 @@ const tweetsRoutes  = express.Router();
 
 module.exports = function(DataHelpers) {
 
+
+  //Get the tweets. This is where we will need to access our database.
   tweetsRoutes.get("/", function(req, res) {
+    //Accesses the datahelpers with a function. Datahelps was passed in where we get the tweets.
     DataHelpers.getTweets((err, tweets) => {
       if (err) {
         res.status(500).json({ error: err.message });
       } else {
+        //Responds with a json tweets to the / page.
         res.json(tweets);
       }
     });
   });
-
+  
+  //Create a new tweet. Post the Tweets we recieve from the database.
   tweetsRoutes.post("/", function(req, res) {
     if (!req.body.text) {
       res.status(400).json({ error: 'invalid request: no data in POST body'});
       return;
     }
-
+    // Generate Random user
     const user = req.body.user ? req.body.user : userHelper.generateRandomUser();
+    // Create the actual tweet object.
     const tweet = {
       user: user,
       content: {
@@ -32,6 +38,7 @@ module.exports = function(DataHelpers) {
       created_at: Date.now()
     };
 
+    // The action that actually saves the tweet has error logic to check if it worked.
     DataHelpers.saveTweet(tweet, (err) => {
       if (err) {
         res.status(500).json({ error: err.message });
@@ -40,7 +47,6 @@ module.exports = function(DataHelpers) {
       }
     });
   });
-
   return tweetsRoutes;
 
 }
